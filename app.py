668903e3d734e8b9fa65dc00fdf6af8ca7244883
matiassoaresdelima-ctrl <matias.soaresdelima@estudiantes.utec.edu.uy>
2026-07-09 18:34:35 +0000

    Fix: Final structural fix for Render deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '12345_comercio_prod'
+app.config['SECRET_KEY'] = 'comercio_prod_123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comercio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
